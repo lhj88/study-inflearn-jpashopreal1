53a8ac244f47c5df460d928175c67f7dcebff2f2
lhj88 <dlgmlwn777@naver.com>
2021-03-02 13:52:48 +0900

    api 개발 회원 등록 조회 수정

--- src/main/java/jpabook/jpashopreal1/domain/Member.java
+++ src/main/java/jpabook/jpashopreal1/domain/Member.java
@@ -2,10 +2,12 @@ package jpabook.jpashopreal1.domain;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +21,7 @@ public class Member {
     @Column(name="member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded

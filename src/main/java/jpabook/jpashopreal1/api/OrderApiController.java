b079d059e51275834839830b30ebbe55a756edb6
lhj88 <dlgmlwn777@naver.com>
2021-03-04 04:49:00 +0900

    주문조회 및 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashopreal1/api/OrderApiController.java
+++ src/main/java/jpabook/jpashopreal1/api/OrderApiController.java
@@ -1,5 +1,136 @@
 package jpabook.jpashopreal1.api;
 
+import jpabook.jpashopreal1.domain.Address;
+import jpabook.jpashopreal1.domain.Order;
+import jpabook.jpashopreal1.domain.OrderItem;
+import jpabook.jpashopreal1.domain.OrderStatus;
+import jpabook.jpashopreal1.repository.order.query.OrderFlatDto;
+import jpabook.jpashopreal1.repository.order.query.OrderQueryDto;
+import jpabook.jpashopreal1.repository.OrderRepository;
+import jpabook.jpashopreal1.repository.OrderSearch;
+import jpabook.jpashopreal1.repository.order.query.OrderQueryRepository;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
+
+
+    // 주문조회 엔티티 직접 노출
+    /*@GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }*/
+
+    // 주문조회 엔티티를 DTO로 변환
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    // 주문조회 엔티티를 DTO로 변환 - fetch 조인 최적화
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    // **********
+    // 주문조회 엔티티를 DTO로 변환 - 페이징과 한계 돌파
+    // **********
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    // 주문조회 JPA에서 DTO 직접조회
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    // ****
+    // 주문조회 JPA에서 DTO 직접조회 - 컬렉션 조회 최적화
+    // ****
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    // 주문조회 JPA에서 DTO 직접조회 - 플랫 데이터 최적화
+    /*@GetMapping("/api/v6/orders")
+    public List<OrderFlatDto> ordersV6(){
+        return orderQueryRepository.findAllByDto_flat();
+    }*/
+
+    @Data
+    static class OrderDto{
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+            this.orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+        private String itemName;    // 상품명
+        private int orderPrice;     // 주문 가격
+        private int count;          // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getItem().getPrice();
+            this.count = orderItem.getCount();
+        }
+    }
 }
--- src/main/java/jpabook/jpashopreal1/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashopreal1/repository/OrderRepository.java
@@ -108,4 +108,23 @@ public class OrderRepository {
                         " left outer join fetch o.delivery d ", Order.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " left outer join fetch o.member m" +
+                        " left outer join fetch o.delivery d ", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithItem(){
+        return em.createQuery("select distinct o from Order o" +
+                " join fetch o.member " +
+                " join fetch o.delivery " +
+                " join fetch o.orderItems oi " +
+                " join fetch oi.item i ", Order.class)
+                .getResultList();
+    }
 }
